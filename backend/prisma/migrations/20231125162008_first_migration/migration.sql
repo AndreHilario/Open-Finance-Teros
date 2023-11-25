cc53327d22cd466c8a87e61d7dcdc8eb0871195a
AndreHilario <andre18hilario@gmail.com>
2023-11-25 13:21:35 -0300

    feat: add prisma schema and first migration to DB

---
+++ backend/prisma/migrations/20231125162008_first_migration/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "participants" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "logoUrl" TEXT NOT NULL,
+    "discoveryUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "participants_pkey" PRIMARY KEY ("id")
+);
