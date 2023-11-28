f4aa9fda37431f40a6f3e93d97c012b1faed7adb
AndreHilario <andre18hilario@gmail.com>
2023-11-28 19:20:39 -0300

    feat: add reset styles and routes to app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
 
-export default App
+export default App;
