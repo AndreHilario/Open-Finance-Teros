7fff1739fa8b31b9140a8da19c02a0cf22f8e27a
AndreHilario <andre18hilario@gmail.com>
2023-11-28 19:32:37 -0300

    feat: add main page, appointed home and his header

---
+++ frontend/src/constants/Header.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <BackGround>
+        <Title>
+          Open Finance
+        </Title>
+      </BackGround>
+    </HeaderContainer>
+  );
+}
+
+const HeaderContainer = styled.header`
+    font-size: 50px;
+`;
+
+const BackGround = styled.div`
+    background-color: #030322;
+    box-shadow: 0 0 33px 0 rgba(0,0,0,.2);
+    width: 100%;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Title = styled.h1`
+    color: #fff;
+`;
---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+import Header from "../../constants/Header";
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
